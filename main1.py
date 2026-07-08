3bf6fbe9d4f64286575d1caff19d53a63372bb7a
RayyanJadoon <jadoonkrayyan@gmail.com>
2026-07-08 19:15:29 +0100

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,13 @@
+my_set = {1, 2, 3}
+print(my_set)
+
+my_set = {1.0, "Hello", (1, 2, 3)}
+print(my_set)
+
+my_set = set({1, 2, 3, 4, 2, 1})
+print(my_set)
+
+num_set = set([1, 2, 3, 4, 5])
+print(num_set)
+num_set.pop()
+print("after pop:", num_set)
